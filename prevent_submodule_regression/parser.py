f0f81f0e6e218c880b30b2e948a20c0cbc4ef955
Erik Ogan <erik@ogan.net>
2020-08-08 17:00:27 -0700

    First pass at the Parser. It appears to work!

--- prevent_submodule_regression/parser.py
+++ prevent_submodule_regression/parser.py
@@ -1,3 +1,98 @@
+import os
+import re
+import subprocess
+import warnings
+
+
 class Parser:
     def run(self, args):
-        print(f"HERE: {args}")
+        self.chdir_root()
+
+        if not args:
+            args = self.list_of_submodules()
+
+        for path in args:
+            self.process(path)
+
+    def process(self, path):
+        if not self.is_staged(path):
+            return
+
+        current_sha = self.submodule_sha(path)
+
+        if not current_sha:
+            warnings.warn(f"{path} is not a submodule, skipping.")
+            return
+
+        shas = self.past_submodule_shas(path)
+
+        if current_sha in shas:
+            exit(
+                f"ERROR: sha {current_sha} was already committed. This appears to be a regression."
+            )
+        exit(0)
+
+    def submodule_sha(self, path):
+        result = subprocess.run(
+            ["git", "ls-files", "--stage", path], capture_output=True, check=True
+        )
+        parts = result.stdout.decode("utf-8").strip().split(" ")
+        if parts[0] != "160000":
+            return None
+        return parts[1]
+
+    def past_submodule_shas(self, path):
+        result = subprocess.run(
+            ["git", "rev-list", "HEAD", "--", path], capture_output=True, check=True
+        )
+        commits = result.stdout.decode("utf-8").strip().split("\n")
+
+        def extract_sha(commit, path):
+            dir = os.path.dirname(path)
+            base = os.path.basename(path)
+            tree = f"{commit}:{dir}" if dir else commit
+
+            result = subprocess.run(
+                ["git", "ls-tree", tree], capture_output=True, check=True
+            )
+            data = result.stdout.decode("utf-8")
+            pat = re.compile(r"\d{6} \w+ ([0-9a-f]{40})\t" + base + "\n")
+
+            res = pat.search(data)
+            if not res:
+                warnings.warn(f"{path} found in sha {commit}!")
+
+                return res
+            return res[1]
+
+        return [extract_sha(c, path) for c in commits]
+
+    def list_of_submodules(self):
+        # Rely on the fact that the .gitmodules file is in git config format
+        result = subprocess.run(
+            ["git", "config", "--file", ".gitmodules", "--get-regexp", "path"],
+            capture_output=True,
+            check=True,
+        )
+        return [
+            line.split(" ")[1]
+            for line in result.stdout.decode("utf-8").strip().split("\n")
+        ]
+
+    def is_staged(self, path):
+        # There is almost assuredly a better way to do this
+        result = subprocess.run(
+            ["git", "diff", "--cached", "--name-only", "--", path],
+            capture_output=True,
+            check=True,
+        )
+        return not not result.stdout.decode("utf-8").strip()
+
+    def chdir_root(self):
+        # TODO: REMOVE
+        os.chdir("../data-river")
+        return
+        result = subprocess.run(
+            ["git", "rev-parse", "--show-toplevel"], capture_output=True, check=True
+        )
+        os.chdir(result.stdout.decode("utf-8").strip())
